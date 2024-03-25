b26b4bd48da003960291897d6159e0109da5daf8
Harry <Harry@Harry>
2024-03-25 13:07:43 +0530

    .

--- Selenium_Batch_10.30/src/ActionsClass/DoubleClickOnMinus.java
+++ Selenium_Batch_10.30/src/ActionsClass/DoubleClickOnMinus.java
@@ -9,7 +9,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
-public class DoubleClickOnMinus {
+public class DoubleClickOnMinus 
+{
 
 	public static void main(String[] args) throws InterruptedException {
 		WebDriver driver = new ChromeDriver();
